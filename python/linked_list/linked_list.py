11708cb87206945823a54ebfc709885329d38ba1
dina-fouad <dina_albarghouthi@yahoo.com>
2021-08-06 17:08:21 +0300

    solving lab6

---
+++ python/linked_list/linked_list.py
@@ -0,0 +1,92 @@
+
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        current = self.head
+        output = ''
+        while current:
+            output += f"{ {str(current.value)} } ->"
+            current = current.next
+        return output
+
+
+
+
+    def insert(self, value):
+
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+    def includes(self,value):
+
+        current=self.head
+        while current:
+            if current.value==value:
+                return True
+            else :
+                current=current.next
+        return False
+##########################66666##########################################
+
+    def append(self,value):
+        node1=Node(value)
+        if not self.head:
+            self.head=node1
+        else:
+            current=self.head
+            while current.next is not None:
+                current=current.next
+            current.next=node1
+
+    def insert_before(self,value,new_value):
+
+        new_node = Node(new_value)
+        current = self.head
+        if not self.head:
+            self.head = new_node
+        else:
+            if current.value == value:
+                th_node = self.head
+                self.head = new_node
+                new_node.next = th_node
+                return
+            else:
+                    current = self.head
+            while current.next :
+                if current.next.value == value:
+                    th_node = current.next
+                    current.next = new_node
+                    new_node.next = th_node
+                    return
+                else:
+                    current = current.next
+            return
+
+    def insert_after(self, value, new_value):
+
+        new_node = Node(new_value)
+        current = self.head
+        if not self.head:
+                self.head = new_node
+        else:
+            current = self.head
+            while current.next != None:
+                if current.next.value == value:
+                    current = current.next
+                    old_node = current.next
+                    current.next = new_node
+                    new_node.next = old_node
+                    return
+                else:
+                    current = current.next
+            return
+########################777777############################################
---
+++ python/tests/test_linked_list.py
@@ -0,0 +1,100 @@
+
+
+from linked_list.linked_list import LinkedList,Node
+
+def test_instance(): ## 1 ## 1
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert isinstance(ll, LinkedList)
+
+def test_insert():
+    l1=LinkedList()
+    value=100
+    l1.insert(value)
+    actual1=l1.head.value
+    actual2=l1.head.next
+    excepted1=100
+    excepted2=None
+    assert actual1==excepted1
+    assert actual2==excepted2
+
+def test_head():
+    l1=LinkedList()
+    node1=l1.insert('200')
+    assert l1.head.value=='200'
+
+
+
+def test_fisrt_elment():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.head.value == 3
+
+def test_false_value():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.includes(5) == False
+
+def test_true():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.includes(2) == True
+
+def test_str():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.__str__() == "{'3'} ->{'2'} ->{'1'} ->"
+
+########################66666############################################
+
+def test_append():
+    l1=LinkedList()
+    l1.append(10)
+    assert l1.head.value is 10
+    l1.append(20)
+    assert l1.head.next.value is 20
+    assert l1.head.next.next is None
+
+
+def test_insert_before():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert_before(2,3)
+    assert ll.__str__() == "{'3'} ->{'2'} ->{'1'} ->"
+
+def test_insert_before_head():
+    ll = LinkedList()
+    ll.append(5)
+    ll.append(6)
+    ll.insert_before(5,4)
+    assert ll.__str__() == "{'4'} ->{'5'} ->{'6'} ->"
+
+def test_insert_after_last():
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    ll.insert_after(3,4)
+    assert ll.__str__() == "{'1'} ->{'2'} ->{'3'} ->{'4'} ->"
+
+def test_insert_after():
+    ll = LinkedList()
+    ll.insert(5)
+    ll.append(6)
+    ll.insert_after(6,8)
+    ll.insert(7)
+    assert ll.__str__() == "{'7'} ->{'5'} ->{'6'} ->{'8'} ->"
+
+    ########################77777############################################
+
